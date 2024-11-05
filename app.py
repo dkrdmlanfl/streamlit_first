62386ccbcd6d8ba77f113327ccace7b96f5118f7
dkrdmlanfl <dkrdmlanfl123@naver.com>
2024-11-05 13:58:33 +0900

    no message

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ st.header('이게 되네~!>~>!~>!~>!>~!>>!~>>~~>~~>~>~>~>~>~>~>😍😍😍😍'
 
 import yfinance as yf
 
-ticker = yf.Ticker('AAPL')
 
+# 원하는 주식 종목 (예: 애플 AAPL)
+ticker_symbol = 'AAPL'
+ticker = yf.Ticker(ticker_symbol)
+
+# 주식 데이터 (최근 1년 데이터, 일별)
 data = ticker.history(period="1y")
 print(data)
