fbd075ccbec5b3a4dcf514af002d244926ecb617
dkrdmlanfl <dkrdmlanfl123@naver.com>
2024-11-05 13:56:51 +0900

    Feat :애플 1년

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 st.title('😍My First Streamlit App 😍')
 
 st.header('이게 되네~!>~>!~>!~>!>~!>>!~>>~~>~~>~>~>~>~>~>~>😍😍😍😍')
+
+import yfinance as yf
+
+ticker = yf.Ticker('AAPL')
+
+data = ticker.history(period="1y")
+print(data)
